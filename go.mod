5e59841f06f25e68e1c24d69c6b1e0f00d68fe58
ilhomsoliev <solievoliver@gmail.com>
2023-03-12 09:47:47 +0500

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ilhomsoliev/chatgpt-telegram-bot
+
+go 1.19
+
+require (
+	github.com/PullRequestInc/go-gpt3 v1.1.13
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"io"
+	"net/http"
+	"os"
+	"github.com/PullRequestInc/go-gpt3"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w,"Hello")
+	})
+	http.HandleFunc("/ask", func(w http.ResponseWriter, r *http.Request) {
+		
+		// Continuosly read and write message
+		//for {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Printf("could not read body: %s\n", err)
+			}
+
+			questionActual := string(body)
+			// ->
+			apiKey := os.Getenv("API_KEY")
+			if apiKey == "" {
+				log.Fatalln("Api Key is missing")
+			}
+			ctx := context.Background()
+			client := gpt3.NewClient(apiKey)
+			question := &Question{}
+			question.Question = questionActual
+			fmt.Println(questionActual)
+			var message = ""
+			client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
+				Prompt: []string{
+					question.Question,
+				},
+				MaxTokens:   gpt3.IntPtr(3000),
+				Temperature: gpt3.Float32Ptr(0),
+			}, func(resp *gpt3.CompletionResponse) {
+				message1 := string(resp.Choices[0].Text)
+				message = message + message1
+				/*err = conn.WriteMessage(mt, message)
+				if err != nil {
+					log.Println("write failed:", err)
+					return
+				}*/
+			})
+			io.WriteString(w, message)
+			/*err = conn.WriteMessage(mt, []byte(string("END")))
+			if err != nil {
+				log.Println("write failed:", err)
+				return
+			}*/
+		//}
+	})
+
+	fmt.Println("Server has started")
+	http.ListenAndServe(":8080", nil)
+}
+
+type Question struct {
+	Question string `json:"question"`
+}
+
+type Answer struct {
+	Answer string `json:"answer"`
+}
+
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+	}
+}
